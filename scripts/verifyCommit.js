b9fb29360f4728e41c49ee84b189828f44c9ca83
kiwi.zhang <kiwi_zhang93@163.com>
2022-08-30 22:32:40 +0800

    feat: 简单处理lerna publish

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,15 +1,12 @@
 // Invoked on the commit-msg git hook by yorkie.
 const chalk = require('chalk')
-console.info(process)
-console.info(process.env)
 const msgPath = process.env.GIT_PARAMS
-const lifecycleEvent = process.env.npm_lifecycle_event
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 const commitRE =
   /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
-const skipRe = /lerna/g
+const skipRe = /^v/g
 
-if (!commitRE.test(msg) && !skipRe.test(lifecycleEvent)) {
+if (!commitRE.test(msg) && !skipRe.test(msg)) {
   console.log()
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
